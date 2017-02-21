e5392bb19db3238d19c9071c59c22a9804083ee7
lleaff <lleaff@outlook.com>
2017-02-21 17:24:30 +0100

    Text update

---
+++ js/main.js
@@ -0,0 +1,3 @@
+'use strict';
+
+(console.info || console.log)('Hi there! This site is just a small overview of my major realisations. See my GitHub and Stack Overflow for more content.');
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,1 @@
-(console.info || console.log)('Hi there! This site is just a small overview of my major realisations. See my GitHub and Stack Overflow for more content.')
+(console.info || console.log)('Hi there! This site is a small overview of my major realisations. See my GitHub and Stack Overflow for more content or contact me at lleaff@outlook.com if you want to talk about anything.')
