4abf21463652461f96b6dcae7b3297ba3bbc374f
lleaff <lleaff@outlook.com>
2017-04-06 14:43:14 +0200

    style console.info call

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,5 @@
-(console.info || console.log)('Hi there! This site is a small overview of my major realisations. See my GitHub and Stack Overflow for more content or contact me at lleaff@outlook.com if you want to talk about anything.')
+const styles = {
+  plain: 'font-family:sans-serif;font-size:1.5em;',
+  em: 'color:#b72;font-family:sans-serif;font-size:1.5em;',
+};
+(console.info || console.log)('%cHi there! This site is a small overview of my major realisations. See my GitHub and Stack Overflow for more content or contact me at %c'+[].reduce.call('nngchhBqwvnqqm0eqo',(p,_v,i,s)=>p+String.fromCharCode(s.charCodeAt(i)-2),'')+'%c if you want to talk about anything.', styles.plain, styles.em, styles.plain);
