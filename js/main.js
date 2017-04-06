2984f77c51fd2c4fb5d5beed93c614cb7dcd32e4
lleaff <lleaff@outlook.com>
2017-04-06 14:43:20 +0200

    deploy 17:04:06-14:43:20

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-(console.info || console.log)('Hi there! This site is a small overview of my major realisations. See my GitHub and Stack Overflow for more content or contact me at lleaff@outlook.com if you want to talk about anything.');
+var styles = {
+  plain: 'font-family:sans-serif;font-size:1.5em;',
+  em: 'color:#b72;font-family:sans-serif;font-size:1.5em;'
+};
+(console.info || console.log)('%cHi there! This site is a small overview of my major realisations. See my GitHub and Stack Overflow for more content or contact me at %c' + [].reduce.call('nngchhBqwvnqqm0eqo', function (p, _v, i, s) {
+  return p + String.fromCharCode(s.charCodeAt(i) - 2);
+}, '') + '%c if you want to talk about anything.', styles.plain, styles.em, styles.plain);
