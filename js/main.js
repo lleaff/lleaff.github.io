f986a41a89f04592fda48656a66ddb88ec06ca70
lleaff <lleaff@outlook.com>
2017-04-06 14:23:02 +0200

    deploy 17:04:06-14:23:02

---
+++ js/main.js
@@ -0,0 +1,3 @@
+'use strict';
+
+(console.info || console.log)('Hi there! This site is a small overview of my major realisations. See my GitHub and Stack Overflow for more content or contact me at lleaff@outlook.com if you want to talk about anything.');
